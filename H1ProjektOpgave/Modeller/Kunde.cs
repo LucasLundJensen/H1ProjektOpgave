2da9d3954f2dd6889826dbe36ed81177fddca164
Lucas Lund Jensen <lucas.lund@live.dk>
2018-03-15 20:40:32 +0100

    1. Lavet en metode der gør det muligt at hente alt dataen ned fra databasen om Kunderne og bilerne. 2. Tilføjet properties til "Kunde"

--- H1ProjektOpgave/Modeller/Kunde.cs
+++ H1ProjektOpgave/Modeller/Kunde.cs
@@ -10,16 +10,54 @@ namespace H1ProjektOpgave
 {
     class Kunde
     {
+        private string kundeid;
 
+        public string Kundeid
+        {
+            get { return kundeid; }
+            set { kundeid = value; }
+        }
 
-        public static string Create(string Navn, string Email)
+        private string fornavn;
+
+        public string Fornavn
+        {
+            get { return fornavn; }
+            set { fornavn = value; }
+        }
+
+        private string efternavn;
+
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set { efternavn = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private string oprettelsesdato;
+
+        public string Oprettelsesdato
+        {
+            get { return oprettelsesdato; }
+            set { oprettelsesdato = value; }
+        }
+
+        public static string Create(string Fornavn, string Efternavn, string Email)
         {
             DateTime Oprettelsesdato = DateTime.Now;
-            string Creation = ("INSERT INTO Kunde (Navn, Email, KundeOprettelsesdato) " + "Values('" + Navn + "', '" + Email + "', '" + Oprettelsesdato + "');");
+            string Creation = ("INSERT INTO Kunde (Fornavn, Efternavn, Email, KundeOprettelsesdato) " + "Values('" + Fornavn + "', '" + Efternavn + "', '" + Email + "', '" + Oprettelsesdato + "');");
             try
             {
                 DBController.CRUD(Creation);
-                return Convert.ToString("Kunde " + Navn + " er nu oprettet.");
+                return Convert.ToString("Kunde " + Fornavn + " " + Efternavn + " er nu oprettet.");
             }
             catch (Exception e)
             {
@@ -51,7 +89,7 @@ namespace H1ProjektOpgave
             DataTable KundeDataTable = DBController.Select(UserLookup + KundeID);
 
             //return Convert.ToString("");
-            string userProperties = "Kundeoplysninger: \nNavn: " + KundeDataTable.Rows[0]["Navn"].ToString() + "\nID: " + KundeDataTable.Rows[0]["KundeID"].ToString() + "\nEmail: " + KundeDataTable.Rows[0]["Email"].ToString() + "\nOprettelses Dato: " + KundeDataTable.Rows[0]["KundeOprettelsesdato"].ToString();
+            string userProperties = "Kundeoplysninger: \nNavn: " + KundeDataTable.Rows[0]["Fornavn"].ToString() + " " + KundeDataTable.Rows[0]["Efternavn"].ToString() + "\nID: " + KundeDataTable.Rows[0]["KundeID"].ToString() + "\nEmail: " + KundeDataTable.Rows[0]["Email"].ToString() + "\nOprettelses Dato: " + KundeDataTable.Rows[0]["KundeOprettelsesdato"].ToString();
             string bilData = string.Empty;
             for (int i = 0; i < KundeDataTable.Rows.Count; i++)
             {
@@ -68,7 +106,59 @@ namespace H1ProjektOpgave
                 bilData = bilData + "\nBilID: " + nyBil.BilID + "\nRegisterings Nummer: " + nyBil.RegNR + "\nMærke: " + nyBil.Maerke + "\nÅrgang: " + nyBil.Aargang + "\nKilometer Kørt: " + nyBil.Km + "\nBrændstofs Type: " + nyBil.BrandStofType + "\n";
             }
             return userProperties + "\n" + bilData;
-            Console.WriteLine("Test");
+        }
+
+        public static string KundeOversigt(string Option)
+        {
+            string sqlKunde = "SELECT * FROM Kunde;";
+            string sqlBil = "SELECT * FROM Biler;";
+            if (Option == "Kundeoversigt")
+            {
+                DataTable KundeTabel = DBController.Select(sqlKunde);
+
+                string Kunder = string.Empty;
+
+                for (int i = 0; i < KundeTabel.Rows.Count; i++)
+                {
+                    Kunde kunde = new Kunde()
+                    {
+                        Kundeid = Convert.ToString(KundeTabel.Rows[i]["KundeID"]),
+                        Fornavn = Convert.ToString(KundeTabel.Rows[i]["Fornavn"]),
+                        Efternavn = Convert.ToString(KundeTabel.Rows[i]["Efternavn"]),
+                        Email = Convert.ToString(KundeTabel.Rows[i]["Email"]),
+                        Oprettelsesdato = Convert.ToString(KundeTabel.Rows[i]["KundeOprettelsesdato"])
+                    };
+                    Kunder = Kunder + "\nKundeID: " + kunde.Kundeid + "\nNavn " + kunde.Fornavn + " " + kunde.Efternavn + "\nEmail: " + kunde.Email + "\nOprettelses Dato: " + kunde.Oprettelsesdato + "\n";
+                }
+                return Kunder;
+            }
+            else if (Option == "Biloversigt")
+            {
+                DataTable BilTabel = DBController.Select(sqlBil);
+
+                string Biler = string.Empty;
+
+                for (int i = 0; i < BilTabel.Rows.Count; i++)
+                {
+                    Modeller.Biler nyBil = new Modeller.Biler()
+                    {
+                        BilID = Convert.ToInt32(BilTabel.Rows[i]["BilID"]),
+                        RegNR = Convert.ToInt32(BilTabel.Rows[i]["RegNr"]),
+                        Maerke = BilTabel.Rows[i]["Mærke"].ToString(),
+                        Aargang = BilTabel.Rows[i]["ModelÅrgang"].ToString(),
+                        Km = Convert.ToInt32(BilTabel.Rows[i]["KmKørt"]),
+                        BrandStofType = BilTabel.Rows[i]["Brændstofstype"].ToString(),
+
+                    };
+                    Biler = Biler + "\nBilID: " + nyBil.BilID + "\nRegisterings Nummer: " + nyBil.RegNR + "\nMærke: " + nyBil.Maerke + "\nÅrgang: " + nyBil.Aargang + "\nKilometer Kørt: " + nyBil.Km + "\nBrændstofs Type: " + nyBil.BrandStofType + "\n";
+                }
+                return Biler;
+            }
+            else
+            {
+                return Convert.ToString(false);
+
+            }
         }
     }
 }
--- H1ProjektOpgave/Program.cs
+++ H1ProjektOpgave/Program.cs
@@ -12,8 +12,6 @@ namespace H1ProjektOpgave
         {
             //Skaber connection til databasen.
 
-            Console.WriteLine(Kunde.Overview(1));
-
             Console.ReadKey();
         }
     }
